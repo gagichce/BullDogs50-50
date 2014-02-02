0f9a788d4583f2fd44b42189d55d60699e23f8e5
gagichce <gagichce@mcmaster.ca>
2014-02-01 23:10:08 -0500

    added the one ticket for 2 dollars bundle

---
+++ BullDogs50-50/OneForTwoBundle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullDogs50_50
+{
+    class OneForTwoBundle : Bundle
+    {
+        public const int setValue = 2;
+        public const int bundleValue = 48;
+        public const int ticketsPerSet = 1;
+        public const int setsPerBundle = 24;
+
+        public OneForTwoBundle()
+        {
+            CreationTime = DateTime.Now;
+        }
+
+    }
+}
---
+++ BullDogs50-50/ThreeForFiveBundle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullDogs50_50
+{
+    class ThreeForFiveBundle : Bundle
+    {
+        public const int setValue = 5;
+        public const int bundleValue = 40;
+        public const int ticketsPerSet = 3;
+        public const int setsPerBundle = 8;
+
+        public ThreeForFiveBundle()
+        {
+            CreationTime = DateTime.Now; 
+        }
+    }
+}

a12ea0a843483bf11abdd07f3d43ab002538e584
gagichce <gagichce@mcmaster.ca>
2014-02-01 22:53:32 -0500

    added constructors for the different styles of bundles, added a builder which makes nice bundles

--- BullDogs50-50/Bundle.cs
+++ BullDogs50-50/Bundle.cs
@@ -13,6 +13,22 @@ namespace BullDogs50_50
         private int ticketsPerSet;
         private int setsPerBundle;
 
+        public Bundle(int setValue, int ticketsPerBundle)
+        {
+            this.ticketsPerSet = 1;
+            this.setValue = setValue;
+            this.setsPerBundle = ticketsPerSet;
+            this.bundleValue = ticketsPerBundle * this.setValue;
+        }
+
+        public Bundle(int ticketsPerSet, int setValue, int setsPerBundle)
+        {
+            this.ticketsPerSet = ticketsPerSet;
+            this.setValue = setValue;
+            this.setsPerBundle = setsPerBundle;
+            this.bundleValue = this.setsPerBundle * this.setValue;
+        }
+
         public int getSetsPerBundle()
         {
             return setsPerBundle;

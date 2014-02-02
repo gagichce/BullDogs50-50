be39bc669dd030729ea2ce55d07780c33484ad0f
gagichce <gagichce@mcmaster.ca>
2014-02-01 22:21:33 -0500

    initial commit

---
+++ BullDogs50-50/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace BullDogs50_50
+{
+    partial class mainEntryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // mainEntryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 492);
+            this.Name = "mainEntryForm";
+            this.Text = "Hamilton Bulldogs 50/50 Draw";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mainEntryForm_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
